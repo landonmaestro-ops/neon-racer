5ccbb6abb9198667aa07d1fc5374c3d55bd8261d
landonmaestro-ops <landonmaestro@gmail.com>
2026-02-20 10:56:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,56 +6,54 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const players = {};
-const bots = {};
+let players = {};
+let bots = {};
 const TOTAL_CARS = 10;
 
-// Function to maintain 10 total cars
 function updateBots() {
     const playerCount = Object.keys(players).length;
     const botsNeeded = Math.max(0, TOTAL_CARS - playerCount);
-    const currentBotCount = Object.keys(bots).length;
-
-    if (currentBotCount < botsNeeded) {
-        // Add a bot
-        const id = 'bot_' + Math.random().toString(36).substr(2, 9);
-        bots[id] = {
-            x: (Math.random() - 0.5) * 800,
-            z: (Math.random() - 0.5) * 800,
-            rot: Math.random() * Math.PI * 2,
-            color: 0xff0000, // Bots are Red-ish
-            speed: 0.1 + Math.random() * 0.2
-        };
-    } else if (currentBotCount > botsNeeded) {
-        // Remove a bot
+    
+    // Remove extra bots if players joined
+    while (Object.keys(bots).length > botsNeeded) {
         const firstBotId = Object.keys(bots)[0];
         delete bots[firstBotId];
         io.emit('botRemoved', firstBotId);
     }
+
+    // Add bots until we hit the target
+    while (Object.keys(bots).length < botsNeeded) {
+        const id = 'bot_' + Math.random().toString(36).substr(2, 9);
+        const neonColors = [0xff00ff, 0x00ff00, 0xffff00, 0xff0000, 0x00ffff, 0xffa500];
+        bots[id] = {
+            x: (Math.random() - 0.5) * 800,
+            z: (Math.random() - 0.5) * 800,
+            rot: Math.random() * Math.PI * 2,
+            color: neonColors[Math.floor(Math.random() * neonColors.length)],
+            speed: 0.15 + Math.random() * 0.25
+        };
+    }
 }
 
-// Move bots aimlessly
+// Bot Brains: Move and Turn
 setInterval(() => {
     Object.keys(bots).forEach(id => {
         const bot = bots[id];
         bot.x += Math.sin(bot.rot) * bot.speed;
         bot.z += Math.cos(bot.rot) * bot.speed;
-        // Randomly turn bots
         if (Math.random() > 0.98) bot.rot += (Math.random() - 0.5) * 2;
-        // Wrap around boundary
-        if (Math.abs(bot.x) > 500) bot.x *= -0.9;
-        if (Math.abs(bot.z) > 500) bot.z *= -0.9;
+        if (Math.abs(bot.x) > 450 || Math.abs(bot.z) > 450) bot.rot += Math.PI; // Bounce off edges
     });
     io.emit('botUpdate', bots);
 }, 100);
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 0, z: 0, rot: 0, color: 0x00ffff };
+    players[socket.id] = { x: 0, z: 0, rot: 0 };
     updateBots();
     
     socket.emit('currentPlayers', players);
     socket.emit('botUpdate', bots);
-    socket.broadcast.emit('newPlayer', { id: socket.id, info: players[socket.id] });
+    socket.broadcast.emit('newPlayer', { id: socket.id });
 
     socket.on('playerMovement', (data) => {
         if (players[socket.id]) {
@@ -78,4 +76,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('--- SERVER ACTIVE ---'));
+http.listen(PORT, () => console.log('SERVER ONLINE'));
