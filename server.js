e273e35063c897f1ee6b89098aaa5a9657169b2f
landonmaestro-ops <landonmaestro@gmail.com>
2026-02-20 10:25:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-app.use(express.static('./public'));
+// Serve files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const players = {};
 
 io.on('connection', (socket) => {
-    console.log('Player connected: ' + socket.id);
+    console.log('A player connected: ' + socket.id);
+
+    // Create new player entry
     players[socket.id] = { x: 0, z: 0, rot: 0, color: 0x00ffff };
-    
+
+    // Tell all players about the new person
     socket.emit('currentPlayers', players);
     socket.broadcast.emit('newPlayer', { id: socket.id, info: players[socket.id] });
 
+    // Handle Movement
     socket.on('playerMovement', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
@@ -24,13 +29,22 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Handle Chat Messages
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', { id: socket.id, msg: msg });
+    });
+
+    // Handle Disconnect
     socket.on('disconnect', () => {
+        console.log('Player disconnected: ' + socket.id);
         delete players[socket.id];
         io.emit('playerDisconnected', socket.id);
     });
 });
 
-http.listen(3000, () => {
+// Use Render's port or default to 3000
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
     console.log('--- SERVER ACTIVE ---');
-    console.log('Go to: http://localhost:3000');
+    console.log('Running on Port: ' + PORT);
 });
